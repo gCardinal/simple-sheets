44035a2155f6460431ddc9a053d66b1d5ba5c72c
Guyllaume Cardinal <guyllaume.cardinal@gmail.com>
2024-11-01 13:16:30 -0400

    working ui lib

---
+++ libs/ui/index.ts
@@ -0,0 +1,5 @@
+import '@mantine/core/styles/global.css';
+import '@mantine/core/styles/UnstyledButton.css';
+import '@mantine/core/styles/Button.css';
+
+export {Button, MantineProvider as ThemeProvider} from '@mantine/core'
